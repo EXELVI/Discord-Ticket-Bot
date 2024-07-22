d8d9683a096e9bc2585fb288421ad42adce91cea
EXELVI <exelvi@outlook.com>
2024-07-22 13:28:06 +0200

    Update leaderboard.js

--- commands/ticket/leaderboard.js
+++ commands/ticket/leaderboard.js
@@ -26,7 +26,7 @@ module.exports = {
         const databasePromise = await require("../../db.js")
         const db = await databasePromise.db("tickets")
         const client = require("../../client.js")
-        const settings = await db.collection("config").findOne({ serverID: interaction.guild.id })
+        const config = await db.collection("config").findOne({ serverID: interaction.guild.id })
 
         let userstatsList = await db.collection("feedbacks").find().toArray();
 
@@ -52,7 +52,7 @@ module.exports = {
                         leaderboardStars += `**#${i + 1}** `
                 }
 
-                let utente = client.guilds.cache.get(settings.idServer).members.cache.find(x => x.id == leaderboardListStars[i].userID);
+                let utente = client.guilds.cache.get(interaction.guild.id).members.cache.find(x => x.id == leaderboardListStars[i].userID);
                 if (utente) leaderboardStars += `${utente.toString()} - **Average ${calculateAverage(leaderboardListStars[i].feedbacks)}** (Count: ${countProps(leaderboardListStars[i].feedbacks)})\n`
             }
         }
@@ -76,12 +76,143 @@ module.exports = {
                         leaderboardCount += `**#${i + 1}** `
                 }
 
-                let utente = client.guilds.cache.get(settings.idServer).members.cache.find(x => x.id == leaderboardListCount[i].userID);
+                let utente = client.guilds.cache.get(interaction.guild.id).members.cache.find(x => x.id == leaderboardListCount[i].userID);
                 if (utente) leaderboardCount += `${utente.toString()} - **Count ${countProps(leaderboardListStars[i].feedbacks)}** (Average: ${calculateAverage(leaderboardListStars[i].feedbacks)})\n`
             }
         }
 
-        
+
+        let beststaff = "Error"
+
+        let utente = client.guilds.cache.get(interaction.guild.id).members.cache.find(x => x.id == getBestStaff(userstatsList))
+        if (utente) beststaff = utente
+
+        let embed = new Discord.EmbedBuilder()
+            .setTitle(":trophy: Feedback Leaderboard :trophy:")
+            .setColor("#ffc400")
+            .setDescription("Best staff: " + beststaff.toString())
+            .addFields({ name: "Average", value: leaderboardStars }, { name: "Quantity", value: leaderboardCount })
+            .setFooter({ text: `Page ${page}/${totPage}` })
+
+        let button1 = new Discord.ButtonBuilder()
+            .setLabel("Previous")
+            .setStyle(1)
+            .setCustomId("previous")
+
+        let button2 = new Discord.ButtonBuilder()
+            .setLabel("Next")
+            .setStyle(1)
+            .setCustomId("next")
+
+        if (page == 1) button1.setDisabled()
+        if (page == totPage) button2.setDisabled()
+
+        let row = new Discord.ActionRowBuilder()
+            .addComponents(button1)
+            .addComponents(button2)
+
+
+        interaction.reply({ embeds: [embed], components: [row] })
+            .then(msg => {
+                const collector = msg.createMessageComponentCollector()
+                collector.on("collect", i => {
+                    i.deferUpdate()
+
+                    if (i.user.id != interaction.user.id) return i.reply({ content: "You can't use this button", ephemeral: true })
+                        
+                    if (i.customId == "previous") {
+                        page--
+                        if (page < 1) page = 1
+                    }
+                    if (i.customId == "next") {
+                        page++
+                        if (page > totPage) page = totPage
+                    }
+
+                    let leaderboardListStars = userstatsList.sort(function (a, b) { return calculateAverage(b.feedbacks) - calculateAverage(a.feedbacks) })
+                    let leaderboardStars = ""
+
+
+                    for (let i = 7 * (page - 1); i < 7 * page; i++) {
+                        if (leaderboardListStars[i]) {
+                            switch (i) {
+                                case 0:
+                                    leaderboardStars += ":first_place: ";
+                                    break
+                                case 1:
+                                    leaderboardStars += ":second_place: "
+                                    break
+                                case 2:
+                                    leaderboardStars += ":third_place: "
+                                    break
+                                default:
+                                    leaderboardStars += `**#${i + 1}** `
+                            }
+
+                            let utente = client.guilds.cache.get(interaction.guild.id).members.cache.find(x => x.id == leaderboardListStars[i].userID);
+                            if (utente) leaderboardStars += `${utente.toString()} - **Average ${calculateAverage(leaderboardListStars[i].feedbacks)}** (Count: ${countProps(leaderboardListStars[i].feedbacks)})\n`
+                        }
+                    }
+
+                    let leaderboardListCount = userstatsList.sort(function (a, b) { return countProps(b.feedbacks) - countProps(a.feedbacks) })
+                    let leaderboardCount = ""
+
+                    for (let i = 7 * (page - 1); i < 7 * page; i++) {
+                        if (leaderboardListCount[i]) {
+                            switch (i) {
+                                case 0:
+                                    leaderboardCount += ":first_place: ";
+                                    break
+                                case 1:
+                                    leaderboardCount += ":second_place: "
+                                    break
+                                case 2:
+                                    leaderboardCount += ":third_place: "
+                                    break
+                                default:
+                                    leaderboardCount += `**#${i + 1}** `
+                            }
+
+                            let utente = client.guilds.cache.get(interaction.guild.id).members.cache.find(x => x.id == leaderboardListCount[i].userID);
+                            if (utente) leaderboardCount += `${utente.toString()} - **Count ${countProps(leaderboardListStars[i].feedbacks)}** (Average: ${calculateAverage(leaderboardListStars[i].feedbacks)})\n`
+                        }
+                    }
+                    beststaffsort = userstatsList.sort(function (a, b) { return (calculateAverage(b.feedbacks) * countProps(b.feedbacks)) - (calculateAverage(a.feedbacks) * countProps(a.feedbacks)) })
+                    beststaff = "Error"
+
+                    utente = client.guilds.cache.get(interaction.guild.id).members.cache.find(x => x.id == beststaffsort[1].userID)
+                    if (utente) beststaff = utente
+
+
+                    embed = new Discord.EmbedBuilder()
+                        .setTitle(":trophy: Feedback Leaderboard :trophy:")
+                        .setColor("#ffc400")
+                        .setDescription("Best staff: " + beststaff.toString())
+                        .addFields({ name: "Average", value: leaderboardStars }, { name: "Quantity", value: leaderboardCount })
+                        .setFooter({ text: `Page ${page}/${totPage}` })
+
+                    let button1 = new Discord.ButtonBuilder()
+                        .setLabel("Previous")
+                        .setStyle(1)
+                        .setCustomId("previous")
+
+                    let button2 = new Discord.ButtonBuilder()
+                        .setLabel("Next")
+                        .setStyle(1)
+                        .setCustomId("next")
+
+                    if (page == 1) button1.setDisabled()
+                    if (page == totPage) button2.setDisabled()
+
+                    let row = new Discord.ActionRowBuilder()
+                        .addComponents(button1)
+                        .addComponents(button2)
+
+                    interaction.editReply({ embeds: [embed], components: [row] })
+                })
+            })
+
+
 
     },
 };
